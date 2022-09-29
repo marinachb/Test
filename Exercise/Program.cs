43a2c77c96552beb9bd5db7c5ee201e0ca63a41c
Marina <chernobaymarina@yandex.ru>
2022-09-29 14:34:57 +0300

    add task

---
+++ Exercise/Program.cs
@@ -0,0 +1,28 @@
+﻿string[] array1 = new string[4] {"Hello", "2", "world", ":-)"};
+string[] array2 = new string[array1.Length];
+
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void NewArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if(array1[i].Length <= 3)
+            {
+                array2[count] = array1[i];
+                count++;
+            }
+    }
+}
+
+ShowArray(array1);
+NewArray(array1, array2);
+ShowArray(array2);
